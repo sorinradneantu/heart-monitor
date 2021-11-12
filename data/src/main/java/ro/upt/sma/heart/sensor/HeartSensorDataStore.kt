a783fb45160983e7aed3aa3dc4d02ee556f8ba87
sorinradneantu <62335059+sorinradneantu@users.noreply.github.com>
2021-11-12 19:04:21 +0200

    sensors todo's done

--- data/src/main/java/ro/upt/sma/heart/sensor/HeartSensorDataStore.kt
+++ data/src/main/java/ro/upt/sma/heart/sensor/HeartSensorDataStore.kt
@@ -2,6 +2,7 @@ package ro.upt.sma.heart.sensor
 
 
 import android.content.Context
+import android.content.Context.SENSOR_SERVICE
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -11,11 +12,10 @@ import java.util.*
 
 class HeartSensorDataStore(context: Context) : HeartSensorRepository {
 
-    // TODO ("Get sensor manager service")
-    private val sensorManager: SensorManager? = null
 
-    // TODO ("Get a handle for the sensor")
-    private val heartRateSensor: Sensor? = null
+    private val sensorManager: SensorManager = context.getSystemService(SENSOR_SERVICE) as SensorManager
+
+    private val heartRateSensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE)
 
     private val listeners = HashMap<HeartSensorRepository.HeartRateListener, SensorEventListener>()
 
@@ -32,8 +32,7 @@ class HeartSensorDataStore(context: Context) : HeartSensorRepository {
             override fun onAccuracyChanged(sensor: Sensor, i: Int) {}
         }
 
-        TODO("Register sensor event listener with the sensor manager API")
-
+        sensorManager.registerListener(eventListener, heartRateSensor, SensorManager.SENSOR_DELAY_NORMAL)
 
         listeners[listener] = eventListener
     }
